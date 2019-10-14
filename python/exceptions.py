1541b2e7369dec85afaa50854ea5e70ed23ae0dd
jax <jax@looker.com>
2019-10-14 16:57:05 +0100

    error handling

---
+++ python/exceptions.py
@@ -0,0 +1,10 @@
+class LookNotFoundError(Exception):
+    pass
+
+
+class RunInlineQueryError(Exception):
+    pass
+
+
+class ArgumentError(Exception):
+    pass
--- python/run_look_with_filters.py
+++ python/run_look_with_filters.py
@@ -1,43 +1,53 @@
+import exceptions
 import json
 import sys
 from typing import cast, Dict, List, Union
 
-from looker_sdk import client, models
+from looker_sdk import client, models, error
 
-sdk = client.setup("../looker.ini")
+sdk = client.setup("looker.ini")
 
 
-def main():
+def main() -> None:
     """Given a look id, obtain the query behind it and run it with the desired
      filter values.
-    """
+
+    https://docs.looker.com/reference/api-and-integration/api-reference/v3.1/query#implementation_notes_9  
+    shows an example of how filters are defined in the posted body. To set the
+    same filter in this example, the script needs to be run as follows:
+
+    $ python run_look_with_filters.py lookId category.name socks
+    """  # noqa:  B950
+
     look_id = sys.argv[1] if len(sys.argv) > 1 else ""
     filter_args = iter(sys.argv[2:])
-    filter_values: List[Dict[str, str]] = []
+    filters: Dict[str, str] = {}
 
     if not (look_id and len(sys.argv[2:]) > 0 and len(sys.argv[2:]) % 2 == 0):
-        print(
+        raise exceptions.ArgumentError(
             "Please provide: <lookId> <filter_1> <filter_value_1> "
             "<filter_2> <filter_value_2> ..."
         )
-        return
 
     for filter_name in filter_args:
-        filter_value = next(filter_args)
-        filter_values.append({filter_name: filter_value})
+        filters[filter_name] = next(filter_args)
 
     query = get_look_query(int(look_id))
-    for filters in filter_values:
-        results = run_query_with_filter(query, filters)
-        print(f"Query results with filters={filters}:\n{results}", end="\n\n")
+    results = run_query_with_filter(query, filters)
+
+    print(f"Query results with filters={filters}:\n{results}", end="\n\n")
 
 
 def get_look_query(id: int) -> models.Query:
     """Returns the query associated with a given look id.
     """
-    look = sdk.look(id)
-    query = look.query
-    assert isinstance(query, models.Query)
+    try:
+        look = sdk.look(id)
+    except error.SDKError:
+        raise exceptions.LookNotFoundError(f"Error getting Look {id}")
+    else:
+        query = look.query
+        assert isinstance(query, models.Query)
     return query
 
 
@@ -48,8 +58,12 @@ def run_query_with_filter(query: models.Query, filters: Dict[str, str]) -> TJson
     """Runs the specified query with the specified filters.
     """
     request = create_query_request(query, filters)
-    json_ = sdk.run_inline_query("json", request, cache=False)
-    json_resp = cast(TJson, json.loads(json_))
+    try:
+        json_ = sdk.run_inline_query("json", request, cache=False)
+    except error.SDKError:
+        raise exceptions.RunInlineQueryError("Error running query")
+    else:
+        json_resp = cast(TJson, json.loads(json_))
     return json_resp
 
 
