c40ba291867446b4bf9c6e6f428d2fa251e58b6b
jcarah <jesse.carah@looker.com>
2019-12-20 16:21:38 -0500

    pull url from sdk auth object
    Co-Authored-By: Joel Dodge <joeldodge@gmail.com>

--- python/content_validator_comparison.py
+++ python/content_validator_comparison.py
@@ -48,7 +48,7 @@ def get_base_url():
     config = configparser.ConfigParser()
     config.read(config_file)
     full_base_url = config.get('Looker', 'base_url')
-    base_url = full_base_url[:full_base_url.index(":19999")]
+    base_url = sdk.auth.settings.base_url[:full_base_url.index(":19999")]
     return base_url
 
 
