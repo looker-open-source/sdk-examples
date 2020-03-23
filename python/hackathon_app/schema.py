a0f7e5ab6f5f95a9c8b4c4a9852f362620562b14
John Kaster <johnk@looker.com>
2020-03-22 21:13:14 -0700

    Getting close on schema tests

--- python/hackathon_app/schema.py
+++ python/hackathon_app/schema.py
@@ -1,41 +1,119 @@
-from pprint import pprint
+import enum
 from typing import Dict, Generic, List, Optional, Union, Sequence, Type, TypeVar, Any
 import os.path
 from google.oauth2 import service_account  # type: ignore
 import googleapiclient.errors  # type: ignore
 import googleapiclient.discovery  # type: ignore
-import json
+
+
+def sheet_range_header(tab_name: str):
+    """Returns the sheet range for the header row of a named tab"""
+    return f"{tab_name}!1:1"
+
+
+class Difference(enum.Enum):
+    """Comparison differences"""
+    Position = enum.auto()  # Position is different
+    Name = enum.auto()  # Name is different
+    OldName = enum.auto()  # Old name is different
+    Columns = enum.auto()  # Column definitions differ
+    Missing = enum.auto()  # Item is not found in "other"
+
+
+class Delta:
+    name : str = ""
+    old_name : str = ""
+    position: int = 0
+    old_position: int = 0
+    diff: Difference
+
+    def __init__(
+            self,
+            *,
+            name: str = "",
+            old_name: str = "",
+            position: int = 0,
+            old_position: int = 0,
+            diff,
+    ):
+        self.name = name
+        self.old_name = old_name
+        self.position = position
+        self.old_position = old_position
+        self.diff = diff
+
+    def debug(self):
+        result = ""
+        if Difference.Position in self.diff:
+            # This can only be set for columns
+            result += f"{self.column_name} position is {self.old_position}, not {self.position}. "
+        if Difference.Name in self.diff:
+            result += f"'Name {self.old_name}' has been changed to '{self.name}'. "
+        if Difference.OldName in self.diff:
+            result += f"Old Name '{self.old_name}' has been changed to '{self.name}'. "
+        if Difference.Columns in self.diff:
+            result += f"Columns for tab '{self.name}' do not match'. "
+        return result
 
 
 class SchemaError(Exception):
     """Improperly formatted data to deserialize"""
 
 
-class SchemaColumn:
-    name: str
-    old_name: str = ""
+class SchemaName:
+    """Base class for sheet schema"""
+    name: str = ""
+    old_name : str = ""
 
-    def __init__(
-            self,
-            *,
-            line: str,
-    ):
-        parts = line.split("~", maxsplit=2)
+    def __init__(self, *, name: str):
+        parts = name.split("~", maxsplit=2)
         self.name = parts[0]
         if len(parts) > 1:
             self.old_name = parts[1]
+        else:
+            self.old_name = ""
 
-    def debug(
-            self
-    ):
+    def debug(self):
         if self.old_name:
             return f"{self.old_name} -> {self.name}"
         else:
             return self.name
 
+    def compare(self, other: "SchemaName"):
+        delta : List[Delta] = []
+        if other.name != self.name:
+            delta.append(Delta(name=self.name, old_name=other.name, diff = Difference.Name))
+        if other.old_name != self.old_name:
+            delta.append(Delta(name=self.old_name, old_name=other.old_name, diff = Difference.OldName))
+        return delta
 
-class SchemaTab:
-    name: str
+    def to_lines(self):
+        if self.old_name:
+            return f"{self.name}~{self.old_name}"
+        return self.name
+
+class SchemaColumn(SchemaName):
+    """Sheet column schema object"""
+    position: int = 0
+
+    def __init__(self, *, line: str, position: int = 0):
+        super().__init__(name=line)
+        self.position = position
+
+    def compare(self, other: "SchemaColumn"):
+        delta = super().compare(other)
+        if other.position != self.position:
+            delta.append(Delta(name=self.name, old_name=other.old_name, position=self.position, old_position=other.position, diff=Difference.Position))
+        return delta
+
+def find_column(name: str, cols: List[SchemaColumn]) -> SchemaColumn:
+    return next(x for x in cols if x.name == name)
+    # found = [x for x in cols if x.name == name]
+    # found = found[0] if found else None
+    # return found
+
+
+class SchemaTab(SchemaName):
     columns: List[SchemaColumn]
 
     def __init__(
@@ -47,34 +125,55 @@ class SchemaTab:
         if len(parts) < 2:
             raise SchemaError(
                 f"Invalid schema definition. '{line}' should be in the format: 'tabname:col1,col2,col3~oldcol3'")
-        self.name = parts[0]
+        super().__init__(name=parts[0])
         self.columns = []
         columns = parts[1].split(",")
         if len(columns) < 1:
             raise SchemaError(f"{self.name} has no valid columns defined in '{parts[1]}'")
-        for col in columns:
+        for index, col in enumerate(columns):
             if not col:
                 continue
-            self.columns.append(SchemaColumn(line=col))
+            self.columns.append(SchemaColumn(line=col, position=index))
 
     def debug(
             self
     ):
-        result = f"{self.name} has {len(self.columns)} columns:"
+        result = f"{super().debug()} has {len(self.columns)} columns:"
         for col in self.columns:
             result += f"\n\t{col.debug()}"
         return result
 
-    def analyze(self):
+    def compare(self, other: "SchemaTab"):
         """Compare the schema and the tab, returning delta"""
-        pass
+        delta = super().compare(other)
+        colDiff = False
+        for col in self.columns:
+            col2 = find_column(col.name, other.columns)
+            if col2:
+                diffs = col.compare(col2)
+                if len(diffs) > 0:
+                    colDiff = True
+                    for d in diffs:
+                        delta.append(d)
+            else:
+                colDiff = True
+                delta.append(Delta(name=col.name, diff=Difference.Missing))
+
+        if colDiff:
+            delta.append(Delta(name=self.name, diff=Difference.Columns))
+        return delta
+
+    def to_lines(self):
+        names = {c.name for c in self.columns}
+        fields = ",".join(names)
+        lines = f"{super().to_lines()}:{fields}"
+        return lines
 
     def update(self):
         """Update the tab to match the schema"""
         pass
 
 
-# @attr.s(auto_attribs=True, kw_only=True)
 class SchemaSheet:
     tabs: List[SchemaTab]
 
@@ -105,12 +204,55 @@ class SchemaSheet:
             result += f"\n{tab.debug()}"
         return result
 
-    def compare(self, schema: "SchemaSheet"):
-        pass
+    def compare(self, other: "SchemaSheet"):
+        delta : List[Delta] = []
+        return delta
+        # for tab in self.tabs:
+        #     delta.append(tab.compare())
+
+    def to_lines(self):
+        lines = {t.to_lines() for t in self.tabs}
+        return "\n".join(lines)
 
 
-def sheet_range_header(tab_name: str):
-    return f"{tab_name}!1:1"
+class DiffColumn:
+    expected: SchemaColumn
+    actual: SchemaColumn
+    delta: List[Delta]
+
+    def __init__(
+            self,
+            *,
+            expected: SchemaColumn,
+            actual: SchemaColumn,
+    ):
+        self.delta = expected.compare(actual)
+
+
+class DiffTab:
+    tab: str = ""
+    delta = List[Delta]
+
+    def __init__(
+            self,
+            *,
+            expected: SchemaTab,
+            actual: SchemaTab,
+    ):
+        self.delta = expected.compare(actual)
+
+
+class DiffSchema:
+    delta: List[Delta]
+
+    def __init__(
+            self,
+            *,
+            expected: SchemaSheet,
+            actual: SchemaSheet,
+    ):
+        self.delta = expected.compare(actual)
+
 
 
 class SchemaReader:
@@ -164,6 +306,7 @@ class SchemaReader:
 
     def debug(self):
         return self.schema.debug()
+
 
 
 if __name__ == "__main__":
--- python/hackathon_app/tests/integration/test_schema.py
+++ python/hackathon_app/tests/integration/test_schema.py
@@ -2,19 +2,20 @@ import schema
 import sheets
 
 
-# def test_read_schema(sheets: Sheets, test_schema):
-def test_read_schema(test_schema):
-    """SchemaSheet should read parse all schema."""
+def test_parse_schema(test_schema):
+    """SchemaSheet should parse all schema."""
     actual = schema.SchemaSheet(lines=test_schema)
+    lines = actual.to_lines()
     assert isinstance(actual, schema.SchemaSheet)
     assert len(actual.tabs) > 0
+    assert test_schema == lines
 
 
 def test_schema_compare(test_schema):
-    """SchemaSheet should read parse all schema."""
-    code = sheets.get_model_schema()
+    """SchemaSheet should compare all schema."""
+    model = sheets.get_model_schema()
     actual = schema.SchemaSheet(lines=test_schema)
-    assert isinstance(code, schema.SchemaSheet)
+    assert isinstance(model, schema.SchemaSheet)
     assert isinstance(actual, schema.SchemaSheet)
     assert len(actual.tabs) > 0
 
@@ -23,4 +24,6 @@ def test_schema_reader(create_test_sheet, cred_file):
     """SchemaSheet should read parse all schema."""
     spreadsheet_id = create_test_sheet["spreadsheetId"]
     reader = schema.SchemaReader(spreadsheet_id=spreadsheet_id, cred_file=cred_file)
-    print(reader.debug())
+    model = sheets.get_model_schema()
+    delta = model.compare(reader.schema)
+    assert len(delta) == 0
