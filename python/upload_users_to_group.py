45b1129fc5dd5d609f1705a1c380f3f46116310c
eric-lyons <eric.lyons@looker.com>
2021-07-30 19:27:01 +0000

    Upload CSV and add users to a Group

--- python/upload_users_to_group.py
+++ python/upload_users_to_group.py
@@ -8,8 +8,9 @@ sdk = looker_sdk.init40("../looker.ini")
 
 #### GO TO ADMIN --> GROUPS AND FIND THE GROUP ID YOU WANT TO ADD THE PEOPLE TO. ADD IT BELOW
 ### Alternative would be to use the search groups endpoint
+### Depending on the cleanliness of your source of emails, you may want to add more error handling
+### EG check for structure, add users without Looker accounts to an output file, or even pass them into another endpoint where you create an account. 
 
-sdk.search_groups()
 def add_csv_of_users_to_group(group_id:int, file_path:str):
     data = []
     i=0
@@ -18,21 +19,24 @@ def add_csv_of_users_to_group(group_id:int, file_path:str):
         for row in reader:
             data.append(str(row[i]))
 
-    ### loops through list and searches user
-    ### grabs user id and passes that through add user to group        
+    ## loops through list and searches user
+    ## grabs user id and passes that through add user to group        
     try:
         for email in data:
             for user in sdk.search_users(email=email):
-                sdk.add_group_user(group_id=group_id, body=models40.GroupIdForGroupUserInclusion(user_id= user.id))
+                #print(user.email)
+                if user.id:
+                    sdk.add_group_user(group_id=group_id, body=models40.GroupIdForGroupUserInclusion(user_id= user.id))
+                else:
+                    pass
+
     except KeyError:
         print('Key error \n')
-        print(email)
         pass
     except TypeError:
         print('Type error \n')
-        print(email)
         pass
     except IndexError:
         print('Index error \n')
-        print(email)
         pass
+add_csv_of_users_to_group(121, "test.csv")
